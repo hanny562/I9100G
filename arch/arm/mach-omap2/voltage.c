fa3fc0f3d398116f3cb4fb1275aa9ca2d84ee9ea
Hanny Liu <hanny562@hotmail.com>
2012-03-28 19:33:06 +0800

    OPP : Update OPP's voltage

--- arch/arm/mach-omap2/voltage.c
+++ arch/arm/mach-omap2/voltage.c
@@ -394,26 +394,26 @@ static struct omap_volt_data omap44xx_vdd_mpu_volt_data[] = {
 	{.volt_nominal = 1011000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C, .abb_type = NOMINAL_OPP},
 	{.volt_nominal = 1025000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C, .abb_type = NOMINAL_OPP},
 	{.volt_nominal = 1200000, .sr_errminlimit = 0xF9, .vp_errgain = 0x16, .abb_type = NOMINAL_OPP},
-	{.volt_nominal = 1313000, .sr_errminlimit = 0xFA, .vp_errgain = 0x23, .abb_type = NOMINAL_OPP},
-	{.volt_nominal = 1374000, .sr_errminlimit = 0xFA, .vp_errgain = 0x27, .abb_type = FAST_OPP},
-	{.volt_nominal = 1375000, .sr_errminlimit = 0xFA, .vp_errgain = 0x27, .abb_type = FAST_OPP},
+	{.volt_nominal = 1325000, .sr_errminlimit = 0xFA, .vp_errgain = 0x23, .abb_type = NOMINAL_OPP},
+	{.volt_nominal = 1387000, .sr_errminlimit = 0xFA, .vp_errgain = 0x27, .abb_type = FAST_OPP},
+	{.volt_nominal = 1388000, .sr_errminlimit = 0xFA, .vp_errgain = 0x27, .abb_type = FAST_OPP},
 };
 
 static struct omap_volt_data omap44xx_vdd_iva_volt_data[] = {
-	{.volt_nominal = 1011000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C, .abb_type = NOMINAL_OPP},
-	{.volt_nominal = 1013000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C, .abb_type = NOMINAL_OPP},
-	{.volt_nominal = 1188000, .sr_errminlimit = 0xF9, .vp_errgain = 0x16, .abb_type = NOMINAL_OPP},
+	{.volt_nominal = 948000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C, .abb_type = NOMINAL_OPP},
+	{.volt_nominal = 950000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C, .abb_type = NOMINAL_OPP},
+	{.volt_nominal = 1114000, .sr_errminlimit = 0xF9, .vp_errgain = 0x16, .abb_type = NOMINAL_OPP},
 #ifdef CONFIG_OMAP_ABB_DEFAULT_IVA_FBB
-	{.volt_nominal = 1300000, .sr_errminlimit = 0xFA, .vp_errgain = 0x23, .abb_type = FAST_OPP},
+	{.volt_nominal = 1291000, .sr_errminlimit = 0xFA, .vp_errgain = 0x23, .abb_type = FAST_OPP},
 #else
-	{.volt_nominal = 1300000, .sr_errminlimit = 0xFA, .vp_errgain = 0x23, .abb_type = NOMINAL_OPP},
+	{.volt_nominal = 1291000, .sr_errminlimit = 0xFA, .vp_errgain = 0x23, .abb_type = NOMINAL_OPP},
 #endif
 };
 
 static struct omap_volt_data omap44xx_vdd_core_volt_data[] = {
-	{.volt_nominal = 1011000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C},
-	{.volt_nominal = 1025000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C},
-	{.volt_nominal = 1200000, .sr_errminlimit = 0xF9, .vp_errgain = 0x16},
+	{.volt_nominal = 942000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C},
+	{.volt_nominal = 962000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C},
+	{.volt_nominal = 1127000, .sr_errminlimit = 0xF9, .vp_errgain = 0x16},
 };
 
 /* OMAP 3430 MPU Core VDD dependency table */
@@ -435,20 +435,20 @@ static struct omap_vdd_dep_info omap34xx_vdd1_dep_info[] = {
 
 /* OMAP 4430 MPU Core VDD dependency table */
 static struct omap_vdd_dep_volt omap44xx_vddmpu_vddcore_data[] = {
-	{.main_vdd_volt = 1011000, .dep_vdd_volt = 1025000},
-	{.main_vdd_volt = 1025000, .dep_vdd_volt = 1025000},
-	{.main_vdd_volt = 1200000, .dep_vdd_volt = 1200000},
-	{.main_vdd_volt = 1313000, .dep_vdd_volt = 1200000},
-	{.main_vdd_volt = 1374000, .dep_vdd_volt = 1200000},
-	{.main_vdd_volt = 1375000, .dep_vdd_volt = 1200000},
+	{.main_vdd_volt = 1011000, .dep_vdd_volt = 942000},
+	{.main_vdd_volt = 1025000, .dep_vdd_volt = 962000},
+	{.main_vdd_volt = 1200000, .dep_vdd_volt = 1127000},
+	{.main_vdd_volt = 1325000, .dep_vdd_volt = 1127000},
+	{.main_vdd_volt = 1387000, .dep_vdd_volt = 1127000},
+	{.main_vdd_volt = 1388000, .dep_vdd_volt = 1127000},
 	{.main_vdd_volt = 0, .dep_vdd_volt = 0},
 };
 
 static struct omap_vdd_dep_volt omap44xx_vddiva_vddcore_data[] = {
-	{.main_vdd_volt = 1011000, .dep_vdd_volt = 1025000},
-	{.main_vdd_volt = 1013000, .dep_vdd_volt = 1025000},
-	{.main_vdd_volt = 1188000, .dep_vdd_volt = 1200000},
-	{.main_vdd_volt = 1300000, .dep_vdd_volt = 1200000},
+	{.main_vdd_volt = 948000, .dep_vdd_volt = 942000},
+	{.main_vdd_volt = 950000, .dep_vdd_volt = 962000},
+	{.main_vdd_volt = 1114000, .dep_vdd_volt = 1127000},
+	{.main_vdd_volt = 1291000, .dep_vdd_volt = 1127000},
 	{.main_vdd_volt = 0, .dep_vdd_volt = 0},
 };
 
