6a83093a5c0fbc3aa6b805bced9a38b605a9a8b4
Hanny Liu <hanny562@hotmail.com>
2012-03-31 09:44:20 +0800

    Edit verison info

--- init/version.c
+++ init/version.c
@@ -42,6 +42,6 @@ const char linux_banner[] =
 	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char linux_proc_banner[] =
-	"%s version %s" "-" LINUX_CODE_NAME  
+	"%s version %s" "-" LINUX_CODE_NAME
 	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
 	" (" LINUX_COMPILER ") %s\n";
