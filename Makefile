8a68d80a0771b76c320e4028190efd60e5e2cf13
Hanny Liu <hanny562@hotmail.com>
2012-04-06 00:30:38 +0800

    Update Version and edit gitignore

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ VERSION = 2
 PATCHLEVEL = 6
 SUBLEVEL = 35
 EXTRAVERSION = .7
-NAME = Lucifr_1.52uv
+NAME = Lucifr_1.52uv_Final
 
 # *DOCUMENTATION*
 # To see a list of typical targets execute "make help"
--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ KERNEL_PATH="/home/hanny/kernel/Lucifr"
 TOOLCHAIN=""
 ROOTFS_PATH="/home/hanny/kernel/initramfs"
 
-FILENAME="Lucifr_1.52uv"
+FILENAME="Lucifr_1.52uv_final"
 export KERNELDIR=$KERNEL_PATH
 
 echo "Compiling Kernel"
