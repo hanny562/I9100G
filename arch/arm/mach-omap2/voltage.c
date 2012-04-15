244cf1cc20b02493838343fdbb800d179b383801
Hanny Liu <hanny562@hotmail.com>
2012-04-15 16:18:06 +0800

    Update freq

--- arch/arm/mach-omap2/voltage.c
+++ arch/arm/mach-omap2/voltage.c
@@ -392,7 +392,6 @@ static struct omap_volt_data omap36xx_vdd2_volt_data[] = {
  */
 static struct omap_volt_data omap44xx_vdd_mpu_volt_data[] = {
 	{.volt_nominal = 850000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C, .abb_type = NOMINAL_OPP},
-	{.volt_nominal = 900000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C, .abb_type = NOMINAL_OPP},
 	{.volt_nominal = 950000, .sr_errminlimit = 0xF4, .vp_errgain = 0x0C, .abb_type = NOMINAL_OPP},
 	{.volt_nominal = 1100000, .sr_errminlimit = 0xF9, .vp_errgain = 0x16, .abb_type = NOMINAL_OPP},
 	{.volt_nominal = 1250000, .sr_errminlimit = 0xFA, .vp_errgain = 0x23, .abb_type = NOMINAL_OPP},
@@ -437,7 +436,6 @@ static struct omap_vdd_dep_info omap34xx_vdd1_dep_info[] = {
 /* OMAP 4430 MPU Core VDD dependency table */
 static struct omap_vdd_dep_volt omap44xx_vddmpu_vddcore_data[] = {
 	{.main_vdd_volt = 850000, .dep_vdd_volt = 942000},
-	{.main_vdd_volt = 900000, .dep_vdd_volt = 962000},
 	{.main_vdd_volt = 950000, .dep_vdd_volt = 962000},
 	{.main_vdd_volt = 1100000, .dep_vdd_volt = 1127000},
 	{.main_vdd_volt = 1250000, .dep_vdd_volt = 1127000},
